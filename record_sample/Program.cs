0f0164b5e87d2900fa024de50edc21be67731c4e
tkmbmbr <tkmbmbr@gmail.com>
2020-10-25 07:47:54 +0900

    Add Record type sample

---
+++ record_sample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace record_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personC1 = new PersonC("yuta1", new DateTime(1987, 8, 8));
+            var personC2 = new PersonC("yuta2", new DateTime(1987, 8, 8));
+            Console.WriteLine(personC1.Equals(personC2));
+            Console.WriteLine(personC1.ToString());
+
+            var personR1 = new PersonR("yuta1", new DateTime(1987, 8, 8));
+            var personR2 = new PersonR("yuta2", new DateTime(1987, 8, 8));
+            Console.WriteLine(personR1.Equals(personR2));
+            Console.WriteLine(personR1.ToString());
+        }
+    }
+
+    class PersonC
+    {
+        public PersonC(string name, DateTime birthday) => (Name, Birthday) = (name, birthday);
+
+        public string Name { get; }
+        public DateTime Birthday { get; }
+    }
+
+    record PersonR
+    {
+        public PersonR(string name, DateTime birthday) => (Name, Birthday) = (name, birthday);
+
+        public string Name { get; }
+        public DateTime Birthday { get; }
+    }
+}
